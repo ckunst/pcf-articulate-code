5e9290dd32f3741837fbb6402ea1f9a78f8f5d1e
Cristian Kunst <ckunst@pivotal.io>
2017-03-15 09:23:11 +0100

    Adding getters and setters to the Attendee POJO.

--- src/main/java/io/pivotal/enablement/articulate/model/Attendee.java
+++ src/main/java/io/pivotal/enablement/articulate/model/Attendee.java
@@ -18,6 +18,55 @@ public class Attendee {
 	private String zipCode;
 	private String phoneNumber;
 	private String emailAddress;
-		
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	
 
 }
